c38a73d12e122bb3b6bb04096de4780a15e7bc45
lianpi3 <sonywertky@gmail.com>
2024-08-15 10:37:15 +0330

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace newsWebapp.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        [Route("~/Index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("~/Contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="newsWebapp.Global" Language="C#" %>
