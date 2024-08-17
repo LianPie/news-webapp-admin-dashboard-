cba87c5f48999e4d6caa2bc1de5473183985cd87
lianpi3 <sonywertky@gmail.com>
2024-08-17 18:04:04 +0330

    menu and home screen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using newsWebapp.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +11,14 @@ namespace newsWebapp.Controllers
     [RoutePrefix("Home")]
     public class HomeController : Controller
     {
+        newswebappEntities db = new newswebappEntities();
+
         [Route("~/")]
         [Route("~/Index")]
         public ActionResult Index()
         {
+            ViewBag.newsCount = db.news.Count();
+            ViewBag.views = db.viewlogs.Where(v => DbFunctions.TruncateTime(v.viewdate) == DateTime.Today).Count();
             return View();
         }
 
